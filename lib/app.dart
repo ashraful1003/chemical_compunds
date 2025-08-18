094642877a25996c5568c6cab60fc497b342e3e7
AshrafulAlam <ashraful.alam@brainstation-23.com>
2025-08-18 18:16:46 +0600

    Project Creation

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:chemical_compounds/features/home/presentation/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomePage(),
+    );
+  }
+}
